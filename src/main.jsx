ae5c714382a938538f67d58b034e31ff6219ea37
SohelShahid117 <sohelshahid09@gmail.com>
2024-04-01 21:45:54 +0600

    45-1 What Is SPA, Routing And React Router Setup has been completed nw

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,46 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+//https://reactrouter.com/en/main/start/tutorial
+//45-1 What Is SPA, Routing And React Router Setup
+
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+// const router = createBrowserRouter([
+//   {
+//     path: "/",
+//     element: <div>Hello world!</div>,
+//   },
+// ]);
+
+const router = createBrowserRouter([
+  {
+    path:"/",
+    // element: <p>hello world</p>
+    element: <div>hello world from react router</div>
+  },
+  {
+    path:"/about",
+    element:<div>now I am in about page</div>
+
+  },
+  {
+    path:"/contact",
+    element:<div>Call me right now</div>
+  }
+])
+
+// ReactDOM.createRoot(document.getElementById('root')).render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+// )
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
